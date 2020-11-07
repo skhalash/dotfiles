d94d75f6ca7a74fcb034e63e2287a922603f1baa
Stanislav Khalash <stanislav.khalash@gmail.com>
2020-11-07 23:09:13 +0100

    Added an installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# This installs homebrew itself, and also the command line tools in silent mode
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew upgrade
+
+## oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
