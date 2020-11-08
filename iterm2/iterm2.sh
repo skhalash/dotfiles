81dc0066a7a07320a3cf8bfde9d6209aeae635bb
Stanislav Khalash <stanislav.khalash@sap.com>
2020-11-08 11:14:49 +0100

    Fixed the iterm script

---
+++ iterm2/iterm2.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Install PowerLine fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# Install iTerm2
+brew update
+brew upgrade
+brew cask install iterm2
+
+# Specify the preferences directory
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$SCRIPT_PATH/com.googlecode.iterm2.plist"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
