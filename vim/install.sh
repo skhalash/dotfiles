47d0bf67881d3679c817f2216c55655df3b9eaa4
Stanislav Khalash <stanislav.khalash@sap.com>
2021-08-17 21:56:30 +0200

    Add Vim8 packages as submodules

--- vim/install.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ~/.vim/pack
-git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
