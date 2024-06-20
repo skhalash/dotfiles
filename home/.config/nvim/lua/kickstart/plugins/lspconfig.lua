6565367f18d678685a162a4dfbdc7def98968a59
Stanislav Khalash <stanislav.khalash@sap.com>
2024-06-20 14:47:56 +0200

    Enable inlay hints

--- home/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ home/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -162,7 +162,16 @@ return {
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				gopls = {},
+				gopls = {
+					settings = {
+						gopls = {
+							["ui.inlayhint.hints"] = {
+								compositeLiteralFields = true,
+								parameterNames = true,
+							},
+						},
+					},
+				},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
