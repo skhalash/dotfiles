7e5477cba5e59857052047804ab1fb696f176f92
Stanislav Khalash <stanislav.khalash@sap.com>
2024-06-26 14:56:12 +0200

    Oil search for hidden files

--- home/.config/nvim/lua/kickstart/plugins/oil.lua
+++ home/.config/nvim/lua/kickstart/plugins/oil.lua
@@ -1,7 +1,23 @@
 return {
 	"stevearc/oil.nvim",
 	config = function()
-		require("oil").setup({ keymaps = { ["<Esc>"] = "actions.close" } })
+		require("oil").setup({
+			keymaps = {
+				["<Esc>"] = "actions.close",
+			},
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+				-- This function defines what is considered a "hidden" file
+				is_hidden_file = function(name)
+					return vim.startswith(name, ".")
+				end,
+				-- This function defines what will never be shown, even when `show_hidden` is set
+				is_always_hidden = function(name)
+					return vim.startswith(name, ".git")
+				end,
+			},
+		})
 	end,
 	keys = {
 		{ "=", "<cmd>Oil<cr>", mode = "n", desc = "Open Filesystem" },
