522ea34423e2011e775d60df0afa395c3f61a5fb
Stanislav Khalash <stanislav.khalash@sap.com>
2024-07-08 23:10:51 +0200

    Add leftover

---
+++ nvim/lua/kickstart/plugins/neogit.lua
@@ -0,0 +1,11 @@
+return {
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+
+		"nvim-telescope/telescope.nvim", -- optional
+	},
+	config = true,
+}
+-- vim: ts=2 sts=2 sw=2 et
