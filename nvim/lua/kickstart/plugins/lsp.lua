59f820adeccd61a95e28cc780229b3073d625895
Stanislav Khalash <stanislav.khalash@sap.com>
2024-07-12 21:30:15 +0200

    Remap inlay hints keymap

--- nvim/lua/kickstart/plugins/lsp.lua
+++ nvim/lua/kickstart/plugins/lsp.lua
@@ -137,9 +137,9 @@ return {
 					--
 					-- This may be unwanted, since they displace some of your code
 					if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-						map("<leader>th", function()
+						map("<leader>ti", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-						end, "[T]oggle Inlay [H]ints")
+						end, "[T]oggle [I]nlay Hints")
 					end
 				end,
 			})
