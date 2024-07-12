d7049c37746dd5aa7fe559d39c81ee0902c06ad8
Stanislav Khalash <stanislav.khalash@sap.com>
2024-07-12 14:53:00 +0200

    Add lazygit nvim plugin

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -55,6 +55,8 @@ require("lazy").setup({
 	require("kickstart/plugins/debug"),
 
 	require("kickstart/plugins/test"),
+
+	require("kickstart/plugins/lazygit"),
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
